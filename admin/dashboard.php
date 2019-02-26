13a0d130868489e4bf4476becfea9dfa95fe827c
Marcos Antonio Cerqueira <marcosfullstack@gmail.com>
2019-02-26 05:53:52 -0300

    mudando estrutura da pasta para padrão MVC

--- admin/dashboard.php
+++
@@ -1,2 +0,0 @@
-// aqui vai código para painel de configuração do blog
-// decidir como vai funcionar as chamadas
--- admin/index.php
+++
@@ -1,2 +0,0 @@
-// aqui vai código para configuração de acesso a página dashboard
-// decidir como vai funcionar as chamadas
